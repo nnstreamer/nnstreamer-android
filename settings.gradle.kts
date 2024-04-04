5f66deccba0ba57d1e6ae7f458b183f9f8fa3990
Wook Song <wook16.song@samsung.com>
2024-04-04 18:57:56 +0900

    Gradle: Add Gradle Kotlin DSL files
    This patch adds Gradle Kotlin DSL files for an empty root project.
    
    Signed-off-by: Wook Song <wook16.song@samsung.com>

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
+
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "nnstreamer-android"
