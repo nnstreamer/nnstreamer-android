1a91e4ab1d8bf0ea9a2c3b4db00153c5058b6e6f
Wook Song <wook16.song@samsung.com>
2024-04-09 19:24:06 +0900

    Gradle/NNSApi: Import ML API including NNStreamer
    This patch imports Java implementations of ML API. This subproject
    also imports native libraries such as NNStreamer, NNStreamer Edge,
    and GStreamer to bring the ML API into the Android space
    
    Signed-off-by: Wook Song <wook16.song@samsung.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.xyz.simple.git) apply false
     alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
 }
