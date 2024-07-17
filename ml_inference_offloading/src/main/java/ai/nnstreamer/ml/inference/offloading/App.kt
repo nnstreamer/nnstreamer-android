29b7583b42ea43813e5f99009fd93633f57b42d4
Wook Song <wook16.song@samsung.com>
2024-07-17 14:53:16 +0900

    App/DI: Introduce Dagger2 to inject dependencies between classes
    This patch introduces Dagger2, the compile-time dependency injection
    framework, to manage dependencies between Android components and related
    classes.
    
    Signed-off-by: Wook Song <wook16.song@samsung.com>

--- ml_inference_offloading/src/main/java/ai/nnstreamer/ml/inference/offloading/App.kt
+++ ml_inference_offloading/src/main/java/ai/nnstreamer/ml/inference/offloading/App.kt
@@ -1,18 +1,25 @@
 package ai.nnstreamer.ml.inference.offloading
 
+import ai.nnstreamer.ml.inference.offloading.di.AppComponent
+import ai.nnstreamer.ml.inference.offloading.di.DaggerAppComponent
 import android.app.Application
 import android.content.Context
 
 class App : Application() {
-    init{
+    init {
         instance = this
     }
 
     companion object {
         lateinit var instance: App
 
-        fun context() : Context {
+        fun context(): Context {
             return instance.applicationContext
         }
     }
+
+    val appComponent: AppComponent by lazy {
+        DaggerAppComponent.factory().create(context())
+    }
+
 }
--- ml_inference_offloading/src/main/java/ai/nnstreamer/ml/inference/offloading/data/OfflineModelsRepository.kt
+++ ml_inference_offloading/src/main/java/ai/nnstreamer/ml/inference/offloading/data/OfflineModelsRepository.kt
@@ -1,12 +1,16 @@
 package ai.nnstreamer.ml.inference.offloading.data
 
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
 
 
-class OfflineModelsRepository(private val modelDao: ModelDao) : ModelsRepository {
+@Singleton
+class OfflineModelsRepository @Inject constructor(private val modelDao: ModelDao) :
+    ModelsRepository {
     override fun getAllModelsStream(): Flow<List<Model>> = modelDao.getAllModels()
     override fun getModelStream(uid: Int): Flow<Model?> = modelDao.getModel(uid)
     override suspend fun insertModel(model: Model) = modelDao.insert(model)
     override suspend fun deleteModel(model: Model) = modelDao.delete(model)
-    override suspend fun updateModel(model: Model) =modelDao.update(model)
+    override suspend fun updateModel(model: Model) = modelDao.update(model)
 }
