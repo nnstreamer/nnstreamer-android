73f1705b925b560e34a4d277185d06bef494a2be
Yelin Jeong <yelini.jeong@samsung.com>
2024-07-22 15:32:39 +0900

    Test: Add instrumented test using pipeline enity
    This patch adds instrumented test using pipeline entity
    
    Signed-off-by: Yelin Jeong <yelini.jeong@samsung.com>

--- ml_inference_offloading/src/androidTest/java/ai/nnstreamer/ml/inference/offloading/LocalDatabaseTest.kt
+++ ml_inference_offloading/src/androidTest/java/ai/nnstreamer/ml/inference/offloading/LocalDatabaseTest.kt
@@ -1,9 +1,11 @@
 package ai.nnstreamer.ml.inference.offloading
 
 import ai.nnstreamer.ml.inference.offloading.data.AppDatabase
+import ai.nnstreamer.ml.inference.offloading.data.Model
 import ai.nnstreamer.ml.inference.offloading.data.ModelDao
 import ai.nnstreamer.ml.inference.offloading.data.PipelineConfig
 import ai.nnstreamer.ml.inference.offloading.data.PipelineConfigDao
+import ai.nnstreamer.ml.inference.offloading.data.PipelineDao
 import ai.nnstreamer.ml.inference.offloading.di.DatabaseModule
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
@@ -25,6 +27,7 @@ class LocalDatabaseTest {
     private var databaseModule = DatabaseModule()
 
     private lateinit var modelDao: ModelDao
+    private lateinit var pipelineDao: PipelineDao
     private lateinit var pipelineConfigDao: PipelineConfigDao
 
     @Before
@@ -32,6 +35,7 @@ class LocalDatabaseTest {
         val context = ApplicationProvider.getApplicationContext<Context>()
         database = databaseModule.provideAppDatabase(context)
         modelDao = database.modelDao()
+        pipelineDao = database.pipelineDao()
         pipelineConfigDao = database.pipelineConfigDao()
     }
 
@@ -58,6 +62,48 @@ class LocalDatabaseTest {
                 assertEquals(srcCaps, it.srcCaps)
                 assertEquals(sinkCaps, it.sinkCaps)
                 assertEquals(properties, it.properties)
+            }
+        }
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun foreignKey() = runTest {
+        val modelName = "test"
+        val model = Model(
+            1,
+            modelName
+        )
+        modelDao.insert(model)
+
+        val srcCaps =
+            "other/tensors,num_tensors=1,format=static,dimensions=(string)3:224:224:1,types=uint8,framerate=0/1"
+        val sinkCaps =
+            "other/tensors,num_tensors=1,format=static,dimensions=(string)1001:1,types=uint8,framerate=0/1"
+        val properties = "framework=tensorflow-lite"
+        val pipelineConfig = PipelineConfig(
+            1,
+            srcCaps,
+            sinkCaps,
+            properties
+        )
+        pipelineConfigDao.insert(pipelineConfig)
+
+        val pipeline = ai.nnstreamer.ml.inference.offloading.data.Pipeline(
+            1,
+            1,
+            1,
+            3030,
+            org.nnsuite.nnstreamer.Pipeline.State.NULL
+        )
+        pipelineDao.insert(pipeline)
+
+        launch {
+            val repeatableFlow = pipelineDao.getPipeline(1).take(1)
+            repeatableFlow.collect {
+                assertEquals(1, it.uid)
+                assertEquals(1, it.modelId)
+                assertEquals(1, it.pipelineConfigId)
             }
         }
     }
