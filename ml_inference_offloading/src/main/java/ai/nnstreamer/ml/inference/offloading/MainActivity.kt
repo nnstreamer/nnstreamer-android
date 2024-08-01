0e96512c5433923d04a7df600822e54777f2502c
Yelin Jeong <yelini.jeong@samsung.com>
2024-08-01 14:13:46 +0900

    App: Remove unused functions and xml files
    This patch removes unused code like recyclerView, model adapter.
    UI is changed to use composable functionss
    
    Signed-off-by: Yelin Jeong <yelini.jeong@samsung.com>

--- ml_inference_offloading/src/main/java/ai/nnstreamer/ml/inference/offloading/MainActivity.kt
+++ ml_inference_offloading/src/main/java/ai/nnstreamer/ml/inference/offloading/MainActivity.kt
@@ -10,11 +10,6 @@ import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -23,67 +18,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import java.io.File
 
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-// todo: Define DTO with generality and extensibility
-data class ModelInfo(
-    val name: String,
-    val filter: String,
-)
-
 class MainActivity : ComponentActivity() {
     private val TAG = "MainActivity"
     private var mService: MainService? = null
-
-    inner class ModelViewHolder(itemView: View) :
-        RecyclerView.ViewHolder(itemView) {
-        fun bind(info: ModelInfo) {
-            val name = itemView.findViewById<TextView>(R.id.name)
-            val start = itemView.findViewById<Button>(R.id.start)
-            val stop = itemView.findViewById<Button>(R.id.stop)
-            val close = itemView.findViewById<Button>(R.id.close)
-            val status = itemView.findViewById<TextView>(R.id.status)
-
-            name.text = info.name
-            start.setOnClickListener(View.OnClickListener {
-                mService?.startServer(info.name, info.filter)
-                mService?.getPort(info.name)?.let { serverPort ->
-                    if (serverPort < 0) status.text = "Failed to start the server"
-                    else status.text = "Listening on port: " + serverPort.toString();
-                }
-            })
-            stop.setOnClickListener(View.OnClickListener {
-                mService?.stopServer(info.name)
-                status.text = resources.getString(R.string.server_paused)
-            })
-            close.setOnClickListener(View.OnClickListener {
-                mService?.closeServer(info.name)
-                status.text = resources.getString(R.string.server_unknown)
-            })
-        }
-    }
-
-    inner class ModelAdapter(private val modelInfos: ArrayList<ModelInfo>) :
-        RecyclerView.Adapter<ModelViewHolder>() {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ModelViewHolder {
-            val inflater = LayoutInflater.from(parent.context)
-            val view = inflater.inflate(R.layout.models, parent, false)
-
-            return ModelViewHolder(view)
-        }
-
-        override fun onBindViewHolder(holder: ModelViewHolder, position: Int) {
-            holder.bind(modelInfos[position])
-        }
-
-        override fun getItemCount(): Int = modelInfos.size
-    }
 
     @Inject
     lateinit var mViewModel: MainViewModel
@@ -124,39 +66,6 @@ class MainActivity : ComponentActivity() {
             }
         }
         startForegroundService(Intent(this, MainService::class.java))
-
-        // todo: Use database instead of just ArrayList
-        val modelList = ArrayList<ModelInfo>()
-        val privateExternalRoot = getExternalFilesDir(null)
-        val privateExternalModels = privateExternalRoot?.resolve("models")
-
-        if (privateExternalModels?.isDirectory == true) {
-            privateExternalModels.list()?.onEach {
-                when (it.toString()) {
-                    // todo: Changes below are temporarily
-                    "mobilenet_v1_1.0_224_quant.tflite" -> {
-                        val mobileNet =
-                            File("$privateExternalModels/$it")
-                        val mobileNetFilter =
-                            "other/tensors,num_tensors=1,format=static,dimensions=(string)3:224:224:1,types=uint8,framerate=0/1 ! " +
-                                    "tensor_filter framework=tensorflow-lite model=" + mobileNet.absolutePath + " ! " +
-                                    "other/tensors,num_tensors=1,format=static,dimensions=(string)1001:1,types=uint8,framerate=0/1"
-                        val mobileNetInfo = ModelInfo("MobileNet", mobileNetFilter)
-                        modelList.add(mobileNetInfo)
-                    }
-
-                    "yolov8s_float32.tflite" -> {
-                        val yolov8 = File("$privateExternalModels/$it")
-                        val yolov8Filter =
-                            "other/tensors,num_tensors=1,format=static,dimensions=3:224:224:1,types=float32,framerate=0/1 ! " +
-                                    "tensor_filter framework=tensorflow-lite model=" + yolov8.absolutePath + " ! " +
-                                    "other/tensors,num_tensors=1,types=float32,format=static,dimensions=1029:84:1,framerate=0/1"
-                        val yolov8Info = ModelInfo("Yolov8", yolov8Filter)
-                        modelList.add(yolov8Info)
-                    }
-                }
-            }
-        }
     }
 
     override fun onStart() {
