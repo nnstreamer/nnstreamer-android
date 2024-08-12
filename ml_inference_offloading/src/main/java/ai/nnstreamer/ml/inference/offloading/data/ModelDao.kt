d2450ec62502712fd3b30fb6b581a228d0d9ddb5
Wook Song <wook16.song@samsung.com>
2024-08-12 19:31:17 +0900

    Data/Model: Add KDoc documentation comments to the Model ROOM DB
    This patch adds KDoc documentation comments to the classes and
    interfaces of the Room database for the Model entity.
    
    Signed-off-by: Wook Song <wook16.song@samsung.com>

--- ml_inference_offloading/src/main/java/ai/nnstreamer/ml/inference/offloading/data/ModelDao.kt
+++ ml_inference_offloading/src/main/java/ai/nnstreamer/ml/inference/offloading/data/ModelDao.kt
@@ -8,20 +8,47 @@ import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
+/** A Data Access Object (DAO) interface for the [Model] class. */
 @Dao
 interface ModelDao {
+    /**
+     * Insert a [Model] into the models table. If the model already exists, replace it.
+     *
+     * @param model the [Model] to be inserted.
+     */
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(model: Model)
 
+    /**
+     * Update the given [Model].
+     *
+     * @param model the [Model] to be updated.
+     */
     @Update
     suspend fun update(model: Model)
 
+    /**
+     * Delete the given [Model] from the models table.
+     *
+     * @param model the [Model] to be deleted.
+     */
     @Delete
     suspend fun delete(model: Model)
 
+    /**
+     * Retrieve a [Model] by [uid] from the models table.
+     *
+     * @param uid the unique identifier that represents a specific [Model] in the table
+     * @return A [Flow] of the matching [Model]s.
+     */
     @Query("SELECT * from models WHERE uid = :uid")
     fun getModel(uid: Int): Flow<Model>
 
+    /**
+     * Retrieve all [Model]s from the models table.
+     *
+     * @return A [Flow] of the list consisting of all [Model]s in the table.
+     */
     @Query("SELECT * from models ORDER BY name ASC")
     fun getAllModels(): Flow<List<Model>>
 }
--- ml_inference_offloading/src/main/java/ai/nnstreamer/ml/inference/offloading/data/ModelRepositoryImpl.kt
+++ ml_inference_offloading/src/main/java/ai/nnstreamer/ml/inference/offloading/data/ModelRepositoryImpl.kt
@@ -5,12 +5,53 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 
+/**
+ * The class that implements the ModelRepository interface.
+ *
+ * @constructor Creates a singleton instance of the ModelRepositoryImpl class.
+ * @pram modelDao The ModelDao object that is used to access the database.
+ */
 @Singleton
 class ModelRepositoryImpl @Inject constructor(private val modelDao: ModelDao) :
     ModelRepository {
+    /**
+     * Get the stream of all [Model]s in the database by using [modelDao].
+     *
+     * @return A [Flow] of a list containing the [Model]s in the database.
+     * @see [ModelRepository.getAllModelsStream]
+     */
     override fun getAllModelsStream(): Flow<List<Model>> = modelDao.getAllModels()
+
+    /**
+     * Get the stream of [Model]s by [uid] from the database by using [modelDao].
+     *
+     * @param uid the unique identifier that represents a specific [Model]
+     * @return A [Flow] of the [Model]s matching the [uid].
+     * @see [ModelRepository.getModelStream]
+     */
     override fun getModelStream(uid: Int): Flow<Model?> = modelDao.getModel(uid)
+
+    /**
+     * Insert a [Model] into the database by using [modelDao].
+     *
+     * @param model the [Model] to be inserted.
+     * @see [ModelRepository.insertModel]
+     */
     override suspend fun insertModel(model: Model) = modelDao.insert(model)
+
+    /**
+     * Delete the given [Model] from the database by using [modelDao].
+     *
+     * @param model the [Model] to be deleted.
+     * @see [ModelRepository.deleteModel]
+     */
     override suspend fun deleteModel(model: Model) = modelDao.delete(model)
+
+    /**
+     * Update the given [Model] in the database by using [modelDao].
+     *
+     * @param model the [Model] to be updated.
+     * @see [ModelRepository.updateModel]
+     */
     override suspend fun updateModel(model: Model) = modelDao.update(model)
 }
