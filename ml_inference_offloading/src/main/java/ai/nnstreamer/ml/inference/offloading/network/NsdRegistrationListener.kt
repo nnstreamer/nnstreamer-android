74d429884dcdb0a62f63ceb759c8f39be6f6250c
Yelin Jeong <yelini.jeong@samsung.com>
2024-08-14 17:50:13 +0900

    App/Network: Add Kdoc for NsdRegistrationListener
    This patch adds Kdoc documentation for NsdRegistrationListener.
    
    Signed-off-by: Yelin Jeong <yelini.jeong@samsung.com>

--- ml_inference_offloading/src/main/java/ai/nnstreamer/ml/inference/offloading/network/NsdRegistrationListener.kt
+++ ml_inference_offloading/src/main/java/ai/nnstreamer/ml/inference/offloading/network/NsdRegistrationListener.kt
@@ -4,26 +4,53 @@ import android.net.nsd.NsdManager
 import android.net.nsd.NsdServiceInfo
 import android.util.Log
 
+/**
+ * A RegistrationListener for the services.
+ *
+ * Listener class for handling events related to network service registration using NSD (Network Service Discovery).
+ */
 class NsdRegistrationListener : NsdManager.RegistrationListener {
     private val TAG = "NsdRegistrationListener"
 
+    /**
+     * A lifecycle callback method that overrides [NsdManager.RegistrationListener.onServiceRegistered].
+     *
+     * @param nsdServiceInfo The information about the registered service.
+     */
     override fun onServiceRegistered(nsdServiceInfo: NsdServiceInfo) {
         Log.i(TAG, "on service registered " + nsdServiceInfo.serviceName)
     }
 
+    /**
+     * A lifecycle callback method that overrides [NsdManager.RegistrationListener.onRegistrationFailed].
+     *
+     * @param nsdServiceInfo The information about the service that failed to register.
+     * @param errorCode The error code indicating the reason for failure.
+     */
     override fun onRegistrationFailed(
-        serviceInfo: NsdServiceInfo,
+        nsdServiceInfo: NsdServiceInfo,
         errorCode: Int,
     ) {
         Log.e(TAG, "registration failed")
     }
 
-    override fun onServiceUnregistered(arg0: NsdServiceInfo) {
+    /**
+     * A lifecycle callback method that overrides [NsdManager.RegistrationListener.onServiceUnregistered].
+     *
+     * @param nsdServiceInfo The information about the unregistered service.
+     */
+    override fun onServiceUnregistered(nsdServiceInfo: NsdServiceInfo) {
         Log.i(TAG, "on service unregistered ")
     }
 
+    /**
+     * A lifecycle callback method that overrides [NsdManager.RegistrationListener.onUnregistrationFailed].
+     *
+     * @param nsdServiceInfo The information about the service that failed to unregister.
+     * @param errorCode The error code indicating the reason for failure.
+     */
     override fun onUnregistrationFailed(
-        serviceInfo: NsdServiceInfo,
+        nsdServiceInfo: NsdServiceInfo,
         errorCode: Int,
     ) {
         Log.e(TAG, "unregistration failed")
