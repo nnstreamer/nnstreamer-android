030b09e6da291ef9fe70042b1296f7bcfa56fa09
Wook Song <wook16.song@samsung.com>
2024-08-16 18:42:12 +0900

    DI/Doc: Add KDoc documentation comments to the file for DI
    This patch adds KDoc documentation comments to the files for Dependency
    Injection framework.
    
    Signed-off-by: Wook Song <wook16.song@samsung.com>

--- ml_inference_offloading/src/main/java/ai/nnstreamer/ml/inference/offloading/di/AppComponent.kt
+++ ml_inference_offloading/src/main/java/ai/nnstreamer/ml/inference/offloading/di/AppComponent.kt
@@ -9,16 +9,51 @@ import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
+/**
+ * Dagger component for dependency injection. This is the main entry point to the Dagger framework.
+ */
 @Singleton
 @Component(modules = [DatabaseModule::class, DataStoreModule::class])
 interface AppComponent {
+    /**
+     * Interface factory for creating instances of AppComponent.
+     */
     @Component.Factory
     interface Factory {
+        /**
+         * Factory method to create an instance of AppComponent.
+         *
+         * @param context the application context.
+         * @return an instance of AppComponent.
+         */
         fun create(@BindsInstance context: Context): AppComponent
     }
 
+    /**
+     * A method to inject dependencies onto a specified activity.
+     *
+     * @param activity the activity to inject dependencies onto.
+     */
     fun inject(activity: MainActivity)
+
+    /**
+     * A method to inject dependencies onto a specified file provider.
+     *
+     * @param fileProvider the file provider to inject dependencies onto.
+     */
     fun inject(fileProvider: ModelFileProvider)
+
+    /**
+     * A method to inject dependencies onto a specified service.
+     *
+     * @param service the service to inject dependencies onto.
+     */
     fun inject(service: MainService)
+
+    /**
+     * A method to inject dependencies onto a specified view model.
+     *
+     * @param viewModel the view model to inject dependencies onto.
+     */
     fun inject(viewModel: MainViewModel)
 }
