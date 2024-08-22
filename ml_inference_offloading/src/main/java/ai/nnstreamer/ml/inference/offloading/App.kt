93ed79a613bb8d2cc11173b59e615dcadb4cf7ca
Wook Song <wook16.song@samsung.com>
2024-08-22 11:00:02 +0900

    App/Doc: Add KDoc documentation comments for the main classes
    This patch adds KDoc documentation to the App and Activity class files,
    which is responsible for providing the Application context and the
    application's main entry point, respectively.
    
    Signed-off-by: Wook Song <wook16.song@samsung.com>

--- ml_inference_offloading/src/main/java/ai/nnstreamer/ml/inference/offloading/App.kt
+++ ml_inference_offloading/src/main/java/ai/nnstreamer/ml/inference/offloading/App.kt
@@ -5,14 +5,29 @@ import ai.nnstreamer.ml.inference.offloading.di.DaggerAppComponent
 import android.app.Application
 import android.content.Context
 
+/**
+ * Application class for providing Application context and DI component.
+ *
+ * @property instance The singleton instance of the application. This is used to provide the application context.
+ * @property appComponent The Dagger component that provides dependencies for the application.
+ */
 class App : Application() {
     init {
         instance = this
     }
 
+    /**
+     * Companion object to provide the application context. This is used to access the application context from other classes.
+     *
+     * @property instance The singleton instance of the application. This is used to provide the application context.
+     */
     companion object {
         lateinit var instance: App
 
+        /**
+         * Function to get the application context.
+         * @return The application context.
+         */
         fun context(): Context {
             return instance.applicationContext
         }
