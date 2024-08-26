0990d1f0f9cfaa89d9dc4ad091225496b370e739
Wook Song <wook16.song@samsung.com>
2024-08-26 09:20:16 +0900

    UI/KDoc: Add KDoc documentation comments for the files in ui/theme
    This patch adds KDoc documentation comments for the Composable functions
    and properties in ui/theme.
    
    Signed-off-by: Wook Song <wook16.song@samsung.com>

--- ml_inference_offloading/src/main/java/ai/nnstreamer/ml/inference/offloading/ui/theme/Color.kt
+++ ml_inference_offloading/src/main/java/ai/nnstreamer/ml/inference/offloading/ui/theme/Color.kt
@@ -2,10 +2,32 @@ package ai.nnstreamer.ml.inference.offloading.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+/**
+ * Light theme colors: Primary.
+ */
 val Purple80 = Color(0xFFD0BCFF)
+
+/**
+ * Light theme colors: Secondary.
+ */
 val PurpleGrey80 = Color(0xFFCCC2DC)
+
+/**
+ * Light theme colors: Tertiary.
+ */
 val Pink80 = Color(0xFFEFB8C8)
 
+/**
+ * Dark theme colors: Primary.
+ */
 val Purple40 = Color(0xFF6650a4)
+
+/**
+ * Dark theme colors: Secondary.
+ */
 val PurpleGrey40 = Color(0xFF625b71)
+
+/**
+ * Dark theme colors: Tertiary.
+ */
 val Pink40 = Color(0xFF7D5260)
